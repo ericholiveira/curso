62219f3b90b8feeb1a5f704b7bc37b6a4248f04f
Erich Oliveira <ericholiveira10@gmail.com>
2018-08-10 10:25:19 -0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,10 @@
+meu_numero = 9
+
+if meu_numero > 3:
+  print("Número é maior do que 3")
+elif meu_numero > 0:
+  print("Número é maior do que 0")
+else:
+  print("Infelizmente número não é maior do que 3 nem maior do que 0")
+
+print('Fim do programa')
