ec545464a732acdd5b0893848a46b762a53936be
Erich Oliveira <ericholiveira10@gmail.com>
2018-08-20 10:54:03 -0300

    task: aula 2

---
+++ aula2.py
@@ -0,0 +1,13 @@
+minha_lista = [1,2,3,4,5,"fwk,jdn,gfkjh", True]
+
+#for elemento in range(1000):
+#  print(f"Essa é a iteração {elemento}")
+#  print("Hello World!!!")
+
+#print("Fim do programa")
+
+def soma_um(numero):
+  resultado = numero + 1
+  return resultado
+
+print(f"====> {soma_um(3)}")
---
+++ revisao_aula1.py
@@ -0,0 +1,22 @@
+1
+1.2
+True
+False
+"fdjkfjkd"
+
+1 + 1
+1 - 1
+1 / 1
+1 * 1
+
+"kdjlkfj"+ "kfdjfk"
+
+True and True
+False or False
+not True
+
+if True:
+  print("Olá")
+elif True:
+  print("Opa")
+else:
